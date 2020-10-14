0f8d630fd17b222773b6ea98851ddd095f74ca47
MarvArray <46500443+MarvArray@users.noreply.github.com>
2020-10-13 22:35:16 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 /**
  * Program to simulate an escape room with multiple doors
  * @author Group 16: Marvin Barajas and Catherine Bui
@@ -8,53 +9,60 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String [] args) {
-        System.out.println("Welcome to the Escape Room.");
-        System.out.println("You must unlock 3 doors to escape...");
+	/**
+	 * Main function to run program
+	 * 
+	 * @param args arguments to be input into main
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Escape Room.");
+		System.out.println("You must unlock 3 doors to escape...");
 
-        for (int i = 0; i < 3; i++) {
-            Random rand = new Random();
-            int doorNum = rand.nextInt(4) + 1;
-    
-            if (doorNum == 1) {
-                Door d = new BasicDoor();
-                System.out.println("You encountered a basic door.");
-                openDoor(d);
-            }
-            else if (doorNum == 2) {
-                Door d = new LockedDoor();
-                System.out.println("You encountered a locked door.");
-                openDoor(d);
-            }
-            else if (doorNum == 3) {
-                Door d = new DeadboltDoor();
-                System.out.println("You encountered a deadbolt door.");
-                openDoor(d);
-            }
-            else if (doorNum == 4) {
-                Door d = new ComboDoor();
-                System.out.println("You encountered a combination door.");
-                openDoor(d);
-            }
-        }
-    }
+		for (int i = 0; i < 3; i++) {
+			Random rand = new Random();
+			int doorNum = rand.nextInt(4) + 1;
 
-    public static void openDoor(Door d) {
-        System.out.println(d.examine());
-        System.out.println(d.menu());
-        int option = CheckInput.getPositiveInt();
-        System.out.println(d.unlock(option));
-        d.open();
-        
-        while (d.open() == false) {
-            System.out.println(d.clue());
-            System.out.println(d.menu());
-            option = CheckInput.getPositiveInt();
-            System.out.println(d.unlock(option));
-            d.open();
-        }
-        if (d.open() == true) {
-            System.out.println(d.success());
-        }
-    }
+			if (doorNum == 1) {
+				Door d = new BasicDoor();
+				System.out.println("You encountered a basic door.");
+				openDoor(d);
+			} else if (doorNum == 2) {
+				Door d = new LockedDoor();
+				System.out.println("You encountered a locked door.");
+				openDoor(d);
+			} else if (doorNum == 3) {
+				Door d = new DeadboltDoor();
+				System.out.println("You encountered a deadbolt door.");
+				openDoor(d);
+			} else if (doorNum == 4) {
+				Door d = new ComboDoor();
+				System.out.println("You encountered a combination door.");
+				openDoor(d);
+			}
+		}
+	}
+
+	/**
+	 * Interacts user with a locked door until it can be opened
+	 * 
+	 * @param d door user will try to unlock
+	 */
+	public static void openDoor(Door d) {
+		System.out.println(d.examine());
+		System.out.println(d.menu());
+		int option = CheckInput.getPositiveInt();
+		System.out.println(d.unlock(option));
+		d.open();
+
+		while (d.open() == false) {
+			System.out.println(d.clue());
+			System.out.println(d.menu());
+			option = CheckInput.getPositiveInt();
+			System.out.println(d.unlock(option));
+			d.open();
+		}
+		if (d.open() == true) {
+			System.out.println(d.success());
+		}
+	}
 }
