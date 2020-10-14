31604e43dbef1193fb14a553e0995981cda8ca62
MarvArray <46500443+MarvArray@users.noreply.github.com>
2020-10-13 19:01:03 -0700

    Add files via upload

---
+++ Door.java
@@ -0,0 +1,36 @@
+/**
+ * Door interface with public abstract functions
+ */
+public interface Door {
+	/**
+	 * Examines a door
+	 * @return emaine result
+	 */
+	public String examine();
+	/**
+	 * Lists user options
+	 * @return user selection
+	 */
+	public String menu();
+	/**
+	 * Unlocks a door
+	 * @param option user selects
+	 * @return unlock result
+	 */
+	public String unlock(int option);
+	/**
+	 * Opens door
+	 * @return return true if open. False otherwise
+	 */
+	public boolean open();
+	/**
+	 * Give user clue if necessary
+	 * @return clue
+	 */
+	public String clue();
+	/**
+	 * Return success message from door
+	 * @return success message from door
+	 */
+	public String success();
+}
