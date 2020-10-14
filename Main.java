9a1b18e92a759864a39ab45de2eb2d3289eb1689
MarvArray <46500443+MarvArray@users.noreply.github.com>
2020-10-13 22:22:14 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/**
+ * Program to simulate an escape room with multiple doors
+ * @author Group 16: Marvin Barajas and Catherine Bui
+ * @version 10/13/2020
+ * Copyright (C) 2020 Marvin Barajas and Catherine Bui. All Rights Reserved.
+ */
+
 import java.util.Random;
 
 public class Main {
@@ -6,33 +13,33 @@ public class Main {
         System.out.println("You must unlock 3 doors to escape...");
 
         Random rand = new Random();
-        int doorNum = rand.nextInt(5) + 1;
+        int doorNum = rand.nextInt(4) + 1, doors = 3;
 
-        if (doorNum == 1) {
-            Door d = new BasicDoor();
-            System.out.println("You encountered a basic door.");
-            openDoor(d);
+        while (doors != 0) {
+        	if (doorNum == 1) {
+                Door d = new BasicDoor();
+                System.out.println("You encountered a basic door.");
+                openDoor(d);
+            }
+            else if (doorNum == 2) {
+                Door d = new LockedDoor();
+                System.out.println("You encountered a locked door.");
+                openDoor(d);
+            }
+            else if (doorNum == 3) {
+                Door d = new DeadboltDoor();
+                System.out.println("You encountered a deadbolt door.");
+                openDoor(d);
+            }
+            else if (doorNum == 4) {
+                Door d = new ComboDoor();
+                System.out.println("You encountered a combination door.");
+                openDoor(d);
+            }
+        	doorNum = rand.nextInt(4) + 1;
+        	doors--;
         }
-        else if (doorNum == 2) {
-            Door d = new LockedDoor();
-            System.out.println("You encountered a locked door.");
-            openDoor(d);
-        }
-        else if (doorNum == 3) {
-            Door d = new DeadboltDoor();
-            System.out.println("You encountered a deadbolt door.");
-            openDoor(d);
-        }
-        else if (doorNum == 4) {
-            Door d = new ComboDoor();
-            System.out.println("You encountered a combination door.");
-            openDoor(d);
-        }
-        else if (doorNum == 5) {
-            Door d = new CodeDoor();
-            System.out.println("You encountered a code door.");
-            openDoor(d);
-        }
+        
     }
 
     public static void openDoor(Door d) {
