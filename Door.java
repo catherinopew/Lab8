0b68c5a2259229fb0bfec55f979a77f827eb6fd9
cat <71743743+catherinopew@users.noreply.github.com>
2020-10-15 11:08:02 -0700

    finalized

--- Door.java
+++ Door.java
@@ -1,35 +1,32 @@
-/**
- * Door interface with public abstract functions
- */
+/** Door interface with public abstract functions */
 public interface Door {
-	/**
-	 * Examines a door
+	/** Examines a door
 	 * @return emaine result
 	 */
 	public String examine();
-	/**
-	 * Lists user options
+
+	/** Lists user options
 	 * @return user selection
 	 */
 	public String menu();
-	/**
-	 * Unlocks a door
+
+	/** Unlocks a door
 	 * @param option user selects
 	 * @return unlock result
 	 */
 	public String unlock(int option);
-	/**
-	 * Opens door
+
+	/** Opens door
 	 * @return return true if open. False otherwise
 	 */
 	public boolean open();
-	/**
-	 * Give user clue if necessary
+
+	/** Give user clue if necessary
 	 * @return clue
 	 */
 	public String clue();
-	/**
-	 * Return success message from door
+
+	/** Return success message from door
 	 * @return success message from door
 	 */
 	public String success();
